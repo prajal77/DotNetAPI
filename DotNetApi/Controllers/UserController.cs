0524bd5f79090db6fcb0297d1f04ec9ccc6479ff
prajal77 <prajal.basnet0@gmail.com>
2024-08-01 23:33:50 +0545

    AddingUserControllerFunction

--- DotNetApi/Controllers/UserController.cs
+++ DotNetApi/Controllers/UserController.cs
@@ -1,6 +1,8 @@
 ﻿using DotNetApi.Data;
+using DotNetApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
 using System.Data;
 
 namespace DotNetApi.Controllers
@@ -21,15 +23,37 @@ namespace DotNetApi.Controllers
             return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
         }
 
-        [HttpGet("GetUsers/{testValue}")]
-        public string[] GetUsers(string testValue)
+        [HttpGet("GetUsers")]
+        public IEnumerable<User> GetUsers()
         {
-            return new string[]
-            {
-                "user1","user2", testValue
-            };
+            string sql = @"
+                        SELECT [UserId],
+                            [FirstName],
+                            [LastName],
+                            [Email],
+                            [Gender],
+                            [Active]  
+                        FROM TutorialAppSchema.Users";
+            IEnumerable < User > users =_dapper.LoadData<User>(sql);
+            return users;
 
         }
-        
+        [HttpGet("GetUsers/{userId}")]
+        public User GetSingleUsers(int userId)
+        {
+
+            string sql = @"
+                        SELECT [UserId],
+                            [FirstName],
+                            [LastName],
+                            [Email],
+                            [Gender],
+                            [Active]  
+                        FROM TutorialAppSchema.Users WHERE UserId = " + userId.ToString();
+            User user = _dapper.LoadDataSingle<User>(sql);
+            return user;
+
+        }
+
     }
 }
---
+++ DotNetApi/Models/UserSalary.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetApi.Models
+{
+    public class UserSalary
+    {
+        public int UserId { get; set; }
+        public decimal Salary { get; set; }
+        public decimal AvgSalary { get; set; }
+
+    }
+}
